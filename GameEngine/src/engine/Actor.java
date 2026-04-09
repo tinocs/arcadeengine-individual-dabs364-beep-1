f60b4eed99e04127e493f922c8f4c7cf6997d580
Eshaan Krishna <ekrishna300@student.fuhsd.org>
2026-04-09 10:16:31 -0700

    HW #1
    completed

--- GameEngine/src/engine/Actor.java
+++ GameEngine/src/engine/Actor.java
@@ -4,5 +4,40 @@ import javafx.scene.image.ImageView;
 
 
 public class Actor extends ImageView {
-    
+    public abstract void act(long now) {
+
+    }
+    public void addedToWorld() {
+
+    }
+
+    public double getWidth() {
+        return getImage().getWidth();
+    }
+    public double getHeight() {
+        return getImage().getHeight();
+    }
+    public <A extends Actor> List<A> getIntersectingObjects(Class<A> cls) {
+        List<A> intersectingObjects = new ArrayList<>();
+        for(A actor : getWorld().getObjects(cls)) {
+            if(actor != this && getBoundsInParent().intersects(actor.getBoundsInParent())) {
+                intersectingObjects.add(actor);
+            }
+        }
+        return intersectingObjects;
+    }
+    public <A extends Actor> getOneIntersectingObject(Class<A> cls) {
+        return getIntersectingObjects(cls).get(0);
+    }
+
+    public World getWorld() {
+        return (World) getParent();
+    }
+
+    public move(double dx, double dy) {
+        setX(getX() + dx);
+        setY(getY() + dy);
+    }
+
+
 }
--- GameEngine/src/engine/World.java
+++ GameEngine/src/engine/World.java
@@ -1,6 +1,64 @@
 package engine;
 
+import javafx.scene.layout.Pane;
+import java.lang.Class;
 
-public class World {
+public class World extends Pane {
+
+    private AnimationTimer timer;
+    private boolean isRunning;
+    private ArrayList<KeyCode> keysPressed;
+    private boolean widthSet;
+    private boolean heightSet;
+
+
+    public abstract void act(long now) {
+
+    }
+    public void add(Actor actor) {
+        getChildren().add(actor);
+    }
+
+    public <A extends Actor> List<A> getObjects(Class<A> cls) {
+        for(A actor : getChildren()) {
+            if(cls.isInstance(actor)) {
+                return (A) actor;
+            }
+        }
+    }
+
+    public <A extends Actor> List<A> getObjectsAt(int x, int y, Class<A> cls) {
+        for(A actor : getObjects(cls)) {
+            if(actor.contains(x, y)) {
+                return (A) actor;
+            }
+        }
+    }
+
+    public boolean isKeyPressed(KeyCode key) {
+        return keysPressed.contains(key);
+    }
+
+    public boolean isStopped() {
+        return !isRunning;
+    }
+
+    public void start() {
+        isRunning = true;
+        timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                act(now);
+                for(Actor actor : getChildren()) {
+                    actor.act(now);
+                }
+            }
+        }
+    }
+
+    public void stop() {
+        isRunning = false;
+        timer.stop();
+    }
 
 }
