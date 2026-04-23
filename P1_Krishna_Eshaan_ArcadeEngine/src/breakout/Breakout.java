c4372f5584fb1c3bea230d10af8289b5bdb6b229
Eshaan Krishna <ekrishna300@student.fuhsd.org>
2026-04-23 14:27:03 -0700

    Title screen

--- P1_Krishna_Eshaan_ArcadeEngine/src/breakout/Breakout.java
+++ P1_Krishna_Eshaan_ArcadeEngine/src/breakout/Breakout.java
@@ -1,8 +1,12 @@
 package breakout;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Breakout extends Application {
@@ -10,12 +14,28 @@ public class Breakout extends Application {
 	@Override
 	public void start(Stage stage) {
 		stage.setTitle("Breakout");
-		BorderPane root = new BorderPane();
-		BallWorld world = new BallWorld();
-		root.setCenter(world);
-		Scene scene = new Scene(root);
-		stage.setScene(scene);
-		world.start();
+
+		BorderPane gameRoot = new BorderPane();
+		BallWorld gameWorld = new BallWorld();
+		gameRoot.setCenter(gameWorld);
+		Scene gameScene = new Scene(gameRoot);
+
+		VBox titleRoot = new VBox();
+		titleRoot.setAlignment(Pos.CENTER);
+		titleRoot.setPrefSize(800, 600);
+		titleRoot.setSpacing(10);
+		Label titleLabel = new Label("Breakout");
+		titleLabel.setStyle("-fx-font-size: 60px; -fx-font-weight: bold;");
+		Button playButton = new Button("Play");
+		titleRoot.getChildren().addAll(titleLabel, playButton);
+		Scene titleScene = new Scene(titleRoot);
+
+		playButton.setOnAction(e -> {
+			stage.setScene(gameScene);
+			gameWorld.start();
+		});
+
+		stage.setScene(titleScene);
 		stage.show();
 	}
 
